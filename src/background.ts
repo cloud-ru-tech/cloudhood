3338018c75ec4b2c45ce03d32d8e7216965f2ed2
Nikita Karpenko <nikkarpa@yandex.ru>
2023-09-14 15:31:06 +0300

    fix: badge on start

--- src/background.ts
+++ src/background.ts
@@ -1,66 +1,25 @@
-import { Profile, RequestHeader } from './entities/request-profile/types';
 import { BrowserStorageKey, ServiceWorkerEvent } from './shared/constants';
-import { setIconBadge } from './shared/utils/setIconBadge';
+import { setBrowserHeaders } from './shared/utils/setBrowserHeaders';
 
 const BADGE_COLOR = '#ffffff';
-
-function getRule(header: RequestHeader) {
-  const allResourceTypes = Object.values(chrome.declarativeNetRequest.ResourceType);
-
-  return {
-    id: header.id,
-    action: {
-      type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
-      requestHeaders: [
-        { header: header.name, value: header.value, operation: chrome.declarativeNetRequest.HeaderOperation.SET },
-      ],
-    },
-    condition: {
-      urlFilter: undefined,
-      resourceTypes: allResourceTypes,
-    },
-  };
-}
 
 function notify(message: ServiceWorkerEvent) {
   if (message === ServiceWorkerEvent.Reload) {
     chrome.storage.local.get(
       [BrowserStorageKey.Profiles, BrowserStorageKey.SelectedProfile, BrowserStorageKey.IsPaused],
-      async result => {
-        const isPaused: boolean = result[BrowserStorageKey.IsPaused];
-        const profiles: Profile[] = JSON.parse(result[BrowserStorageKey.Profiles]);
-        const selectedProfile: string = result[BrowserStorageKey.SelectedProfile];
-        const currentRules = await chrome.declarativeNetRequest.getDynamicRules();
-
-        const profile = profiles.find(p => p.id === selectedProfile);
-
-        const selectedProfileHeaders = profile?.requestHeaders ?? [];
-
-        const activeRules = selectedProfileHeaders.filter(({ disabled, name }) => !disabled && Boolean(name));
-
-        const addRules = !isPaused ? activeRules.map(getRule) : [];
-        const removeRuleIds = currentRules.map(item => item.id);
-
-        try {
-          await chrome.declarativeNetRequest.updateDynamicRules({
-            removeRuleIds,
-            addRules,
-          });
-
-          await setIconBadge({ isPaused, activeRulesCount: activeRules.length });
-        } catch (err) {
-          // eslint-disable-next-line no-console
-          console.log(err);
-        }
-      },
+      setBrowserHeaders,
     );
   }
 }
 
-chrome.storage.local.get(
-  [BrowserStorageKey.Profiles, BrowserStorageKey.SelectedProfile, BrowserStorageKey.IsPaused],
-  result => Object.keys(result).length && notify(ServiceWorkerEvent.Reload),
-);
+chrome.runtime.onStartup.addListener(function () {
+  chrome.storage.local.get(
+    [BrowserStorageKey.Profiles, BrowserStorageKey.SelectedProfile, BrowserStorageKey.IsPaused],
+    async result => {
+      Object.keys(result).length && setBrowserHeaders(result);
+    },
+  );
+});
 
 chrome.action.setBadgeBackgroundColor({ color: BADGE_COLOR });
 
---
+++ src/shared/utils/setBrowserHeaders.ts
@@ -0,0 +1,49 @@
+import { Profile, RequestHeader } from '#entities/request-profile/types';
+import { BrowserStorageKey } from '#shared/constants';
+
+import { setIconBadge } from './setIconBadge';
+
+function getRule(header: RequestHeader) {
+  const allResourceTypes = Object.values(chrome.declarativeNetRequest.ResourceType);
+
+  return {
+    id: header.id,
+    action: {
+      type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+      requestHeaders: [
+        { header: header.name, value: header.value, operation: chrome.declarativeNetRequest.HeaderOperation.SET },
+      ],
+    },
+    condition: {
+      urlFilter: undefined,
+      resourceTypes: allResourceTypes,
+    },
+  };
+}
+
+export async function setBrowserHeaders(result: Record<string, unknown>) {
+  const isPaused = result[BrowserStorageKey.IsPaused] as boolean;
+  const profiles: Profile[] = JSON.parse(result[BrowserStorageKey.Profiles] as string);
+  const selectedProfile = result[BrowserStorageKey.SelectedProfile] as string;
+  const currentRules = await chrome.declarativeNetRequest.getDynamicRules();
+
+  const profile = profiles.find(p => p.id === selectedProfile);
+
+  const selectedProfileHeaders = profile?.requestHeaders ?? [];
+
+  const activeRules = selectedProfileHeaders.filter(({ disabled, name }) => !disabled && Boolean(name));
+
+  const addRules = !isPaused ? activeRules.map(getRule) : [];
+  const removeRuleIds = currentRules.map(item => item.id);
+
+  try {
+    await chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds,
+      addRules,
+    });
+    await setIconBadge({ isPaused, activeRulesCount: activeRules.length });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+}
