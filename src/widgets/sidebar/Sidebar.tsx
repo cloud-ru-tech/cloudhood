2a371672fd96e3fc85cbefea16bcb083ca71dc20
Egor Bibin <evbibin@cloud.ru>
2023-09-14 14:35:10 +0300

    feat(LK-2582): add github link

--- src/widgets/sidebar/Sidebar.tsx
+++ src/widgets/sidebar/Sidebar.tsx
@@ -3,10 +3,13 @@ import { IconButton } from '@mui/material';
 import { useUnit } from 'effector-react';
 
 import { $selectedRequestProfile, profileAdded } from '#entities/request-profile/model';
+import { GuthubIcon } from '#shared/assets/GuthubIcon/GuthubIcon';
 import { SetRequestProfile } from '#widgets/sidebar/components/SetRequestProfile';
 
 import { $profilesName } from './model';
 import * as S from './styled';
+
+const CLOUDHOOD_GITHUB_URL = 'https://github.com/sbercloud/cloudhood/';
 
 export function Sidebar() {
   const [profileNames, selectedProfile, handleAddProfile] = useUnit([
@@ -15,18 +18,26 @@ export function Sidebar() {
     profileAdded,
   ]);
 
+  const handleGithubIconClick = () => window.open(CLOUDHOOD_GITHUB_URL, '_blank')?.focus();
+
   return (
     <S.Wrapper>
-      {profileNames.map((profile, index) => (
-        <SetRequestProfile
-          key={profile.toString()}
-          index={(index + 1).toString()}
-          isSelected={profile === selectedProfile}
-          profile={profile}
-        />
-      ))}
-      <IconButton onClick={handleAddProfile}>
-        <ControlPointIcon />
+      <S.ProfilesWrapper>
+        {profileNames.map((profile, index) => (
+          <SetRequestProfile
+            key={profile.toString()}
+            index={(index + 1).toString()}
+            isSelected={profile === selectedProfile}
+            profile={profile}
+          />
+        ))}
+        <IconButton onClick={handleAddProfile}>
+          <ControlPointIcon />
+        </IconButton>
+      </S.ProfilesWrapper>
+
+      <IconButton onClick={handleGithubIconClick}>
+        <GuthubIcon />
       </IconButton>
     </S.Wrapper>
   );
--- src/widgets/sidebar/styled.ts
+++ src/widgets/sidebar/styled.ts
@@ -3,10 +3,18 @@ import styled from '@emotion/styled';
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 12px;
+
+  padding: 16px 12px 8px 12px;
+`;
+
+export const ProfilesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   align-items: center;
   gap: 12px;
 
   overflow-y: auto;
-
-  padding: 16px 12px;
 `;
