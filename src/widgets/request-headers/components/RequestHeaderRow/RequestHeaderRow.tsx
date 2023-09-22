2a327ac0b46b3443a98e697687f3a9ee3841ba02
Robert <rikuzhin@cloud.ru>
2023-09-22 14:13:42 +0300

    feat(LK-2633): add request headers drag and drop

--- src/widgets/request-headers/components/RequestHeaderRow/RequestHeaderRow.tsx
+++ src/widgets/request-headers/components/RequestHeaderRow/RequestHeaderRow.tsx
@@ -1,18 +1,22 @@
+import { useSortable } from '@dnd-kit/sortable';
 import ClearIcon from '@mui/icons-material/Clear';
 import { Checkbox, IconButton, TextField } from '@mui/material';
 
 import { RequestHeader } from '#entities/request-profile/types';
 import { selectedProfileRequestHeadersRemoved } from '#features/selected-profile-request-headers/remove/model';
+import { DragHandle } from '#features/selected-profile-request-headers/reorder/components';
 import { selectedProfileRequestHeadersUpdated } from '#features/selected-profile-request-headers/update/model';
 
 import * as S from './styled';
 
 export function RequestHeaderRow(props: RequestHeader) {
   const { disabled, name, value, id } = props;
+  const { setNodeRef, listeners, attributes, transition, transform, isDragging } = useSortable({ id });
 
   return (
-    <S.Wrapper>
+    <S.Wrapper ref={setNodeRef} transform={transform} transition={transition} isDragging={isDragging}>
       <S.LeftHeaderActions>
+        <DragHandle listeners={listeners} attributes={attributes} />
         <Checkbox
           color='default'
           checked={!disabled}
--- src/widgets/request-headers/components/RequestHeaderRow/styled.ts
+++ src/widgets/request-headers/components/RequestHeaderRow/styled.ts
@@ -1,9 +1,14 @@
+import { CSS, Transform } from '@dnd-kit/utilities';
 import styled from '@emotion/styled';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<{ transform: Transform | null; isDragging: boolean; transition?: string }>`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+  transform: ${props => CSS.Transform.toString(props.transform)};
+  opacity: ${props => (props.isDragging ? 0 : 1)};
+  transition: ${props => props.transition};
 
   width: 100%;
 `;
@@ -11,5 +16,4 @@ export const Wrapper = styled.div`
 export const LeftHeaderActions = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 8px;
 `;
