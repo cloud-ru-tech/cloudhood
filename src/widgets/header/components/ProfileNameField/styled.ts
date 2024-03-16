dfeeee9f461f6d8bfd93533de54909d4a59f31f2
Нина Пономарева <niponomareva@cloud.ru>
2024-03-17 00:57:18 +0300

    fix(LK-3225): fix styles for profile name field

--- src/widgets/header/components/ProfileNameField/styled.ts
+++ src/widgets/header/components/ProfileNameField/styled.ts
@@ -1,10 +1,14 @@
 import styled from '@emotion/styled';
 import _EditIcon from '@mui/icons-material/Edit';
-import { TextField as _TextField } from '@mui/material';
+import { TextField as _TextField, Typography as _Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
 
 export const EditIcon = styled(_EditIcon)`
   fill: ${grey[100]};
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const TextField = styled(_TextField)`
@@ -20,4 +24,14 @@ export const TextField = styled(_TextField)`
   & .MuiInput-underline:after {
     border-bottom-color: white;
   }
+`;
+
+export const Typography = styled(_Typography)`
+  width: fit-content;
+  max-width: 100%;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  overflow: hidden;
+  word-break: break-all;
 `;
