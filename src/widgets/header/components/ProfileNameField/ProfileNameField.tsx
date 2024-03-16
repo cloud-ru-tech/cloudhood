b11d6892f8152199672e82c565bbd7e7a3f2556f
Нина Пономарева <niponomareva@cloud.ru>
2024-03-17 00:57:18 +0300

    fix(LK-3225): fix styles for profile name field

--- src/widgets/header/components/ProfileNameField/ProfileNameField.tsx
+++ src/widgets/header/components/ProfileNameField/ProfileNameField.tsx
@@ -1,11 +1,24 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Tooltip, TooltipProps } from '@mui/material';
 import { useUnit } from 'effector-react';
-import { KeyboardEvent, useRef, useState } from 'react';
+import { KeyboardEvent, useMemo, useRef, useState } from 'react';
 
 import { $selectedProfile, $selectedProfileIndex } from '#entities/request-profile/model';
 import { setSelectedRequestProfileName } from '#features/selected-profile-update-name/model';
 
-import { EditIcon, TextField } from './styled';
+import { EditIcon, TextField, Typography } from './styled';
+
+const tooltipProps: TooltipProps['slotProps'] = {
+  popper: {
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: [0, -5],
+        },
+      },
+    ],
+  },
+};
 
 export function ProfileNameField() {
   const [isEdited, setIsEdited] = useState<boolean>(false);
@@ -27,24 +40,33 @@ export function ProfileNameField() {
     }
   };
 
+  const profileName = useMemo(
+    () => (profile?.name !== undefined ? profile.name : `Profile ${profileIndex + 1}`),
+    [profile, profileIndex],
+  );
+
   return (
-    <Grid container>
-      <Grid item xs={5}>
+    <Grid container alignItems='center' xs={6} spacing={2} wrap='nowrap'>
+      <Grid item xs={8}>
         {isEdited ? (
           <TextField
             inputRef={ref}
             placeholder='Profile name'
             variant='standard'
-            defaultValue={profile?.name !== undefined ? profile?.name : `Profile ${profileIndex + 1}`}
+            defaultValue={profileName}
             onKeyUp={handleKeyUp}
+            onBlur={toggleEdit}
+            autoFocus
           />
         ) : (
-          <Typography color='white' variant='h6'>
-            {profile?.name || `Profile ${profileIndex + 1}`}
-          </Typography>
+          <Tooltip title={profileName} arrow placement='bottom-start' slotProps={tooltipProps}>
+            <Typography color='white' variant='h6'>
+              {profileName}
+            </Typography>
+          </Tooltip>
         )}
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={4}>
         <EditIcon onClick={toggleEdit} />
       </Grid>
     </Grid>
