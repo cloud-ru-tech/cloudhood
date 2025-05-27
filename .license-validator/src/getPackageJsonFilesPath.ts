620a03e547c0320c25702366d376d719e1ff3c6c
Nikita Ershov <niershov@cloud.ru>
2025-05-27 18:10:23 +0300

    chore(FF-00): upd check license

--- .license-validator/src/getPackageJsonFilesPath.ts
+++
@@ -1,7 +0,0 @@
-import { globSync } from 'glob';
-
-export function getPackageJsonFilesPath() {
-  return globSync(['package.json', '**/*/package.json'], {
-    ignore: ['node_modules/**', '**/*/node_modules/**'],
-  });
-}
