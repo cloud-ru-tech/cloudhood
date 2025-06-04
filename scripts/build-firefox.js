3798a4de55c0504b4a6cdd4479a7a162e5dab711
kirrrrusha <kirilllebedenko@gmail.com>
2025-06-04 23:09:15 +0300

    feat(PROFCOMM-000): update lock, add reinstall script

--- scripts/build-firefox.js
+++ scripts/build-firefox.js
@@ -1,79 +1,35 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
-// Default browser and build dir
-process.env.BROWSER = process.env.BROWSER || 'chrome';
-process.env.BUILD_DIR = process.env.BROWSER === 'firefox' ? 'firefox' : 'chrome';
+process.env.BROWSER = 'firefox';
+process.env.BUILD_DIR = 'firefox';
 
-var WebpackDevServer = require('webpack-dev-server'),
-  webpack = require('webpack'),
-  config = require('../webpack.config'),
-  env = require('./env'),
-  path = require('path');
-
-var options = config.chromeExtensionBoilerplate || {};
-var excludeEntriesToHotReload = options.notHotReload || [];
-
-for (var entryName in config.entry) {
-  if (excludeEntriesToHotReload.indexOf(entryName) === -1) {
-    config.entry[entryName] = [
-      'webpack/hot/dev-server',
-      `webpack-dev-server/client?hot=true&hostname=localhost&port=${env.PORT}`,
-    ].concat(config.entry[entryName]);
-  }
-}
-
-config.plugins = [new webpack.HotModuleReplacementPlugin()].concat(config.plugins || []);
+var webpack = require('webpack'),
+  config = require('../webpack.config');
 
 delete config.chromeExtensionBoilerplate;
 
-var compiler = webpack(config);
+config.mode = 'production';
 
-var server = new WebpackDevServer(
-  {
-    server: {
-      type: 'http',
-    },
-    hot: false,
-    client: false,
-    host: 'localhost',
-    port: env.PORT,
-    static: {
-      directory: path.join(__dirname, `../build/${process.env.BUILD_DIR}`),
-    },
-    devMiddleware: {
-      publicPath: `http://localhost:${env.PORT}/`,
-      writeToDisk: true,
-    },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    allowedHosts: 'all',
-  },
-  compiler,
-);
+webpack(config, function (err, stats) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept();
-}
+  // eslint-disable-next-line no-console
+  console.log(
+    stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+    }),
+  );
 
-// eslint-disable-next-line no-console
-console.log(
-  `Starting ${process.env.BROWSER.toUpperCase()} extension development server (BUILD_DIR: ${
-    process.env.BUILD_DIR
-  }) on http://localhost:${env.PORT}`,
-);
-
-// Display startup info
-// eslint-disable-next-line no-console
-console.log(
-  `Starting ${process.env.BROWSER.toUpperCase()} extension development server (BUILD_DIR: ${
-    process.env.BUILD_DIR
-  }) on http://localhost:${env.PORT}`,
-);
-
-(async () => {
-  await server.start();
-})();
+  // eslint-disable-next-line no-console
+  console.log('\nBuild completed in', stats.endTime - stats.startTime, 'ms');
+});
