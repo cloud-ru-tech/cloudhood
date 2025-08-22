cf0aea67123517116398bf14a1ad20554e074c75
damisenko <damisenko@cloud.ru>
2025-08-22 12:53:35 +0300

    feat(PROFCOMM-720): add tooltip for profiles in sidebar

--- src/widgets/sidebar/components/SetRequestProfile/SetRequestProfile.tsx
+++ src/widgets/sidebar/components/SetRequestProfile/SetRequestProfile.tsx
@@ -1,5 +1,9 @@
+
+import { Tooltip } from '@snack-uikit/tooltip';
+
 import { selectedRequestProfileIdChanged } from '#entities/request-profile/model';
 import { profileColorList } from '#shared/assets/colors';
+import { getProfileNameAbbreviation } from '#widgets/sidebar/helpers/sidebar';
 
 import * as S from './styled';
 
@@ -7,12 +11,13 @@ type SetRequestProfileProps = {
   profileId: string;
   index: number;
   isSelected: boolean;
-  profileNameAbbreviation: string;
+  profileName: string;
 };
 
-export function SetRequestProfile({ profileId, index, isSelected, profileNameAbbreviation }: SetRequestProfileProps) {
+export function SetRequestProfile({ profileId, index, isSelected, profileName }: SetRequestProfileProps) {
   const handleClick = () => selectedRequestProfileIdChanged(profileId);
   const color = profileColorList[index % profileColorList.length];
+  const profileNameAbbreviation = getProfileNameAbbreviation(profileName);
 
   return (
     <S.Circle
@@ -21,7 +26,13 @@ export function SetRequestProfile({ profileId, index, isSelected, profileNameAbb
       color={color.font}
       backgroundColor={color.background}
     >
-      {profileNameAbbreviation.length === 0 ? index + 1 : profileNameAbbreviation}
+      <Tooltip
+        tip={profileName}
+        placement='right'
+        {...(profileName ? {} : { open: false })}
+      >
+        {profileNameAbbreviation.length === 0 ? index + 1 : profileNameAbbreviation}
+      </Tooltip>
     </S.Circle>
   );
 }
