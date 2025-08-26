855a11b48a7ee213c22f57e1404d860c6d22cdd2
Kirill Lebedenko <kialebedenko@cloud.ru>
2025-08-26 09:45:35 +0300

    feat: add remove all URL filters functionality

---
+++ src/features/selected-profile-url-filters/remove-all/model.ts
@@ -0,0 +1,19 @@
+import { attach, createEvent, sample } from 'effector';
+
+import { $requestProfiles, $selectedRequestProfile, profileUpdated } from '#entities/request-profile/model';
+
+export const selectedProfileAllUrlFiltersRemoved = createEvent();
+
+const selectedProfileAllUrlFiltersRemovedFx = attach({
+  source: { profiles: $requestProfiles, selectedProfile: $selectedRequestProfile },
+  effect: ({ profiles, selectedProfile }) => {
+    const profile = profiles.find(p => p.id === selectedProfile);
+    return {
+      ...profile,
+      urlFilters: [],
+    };
+  },
+});
+
+sample({ clock: selectedProfileAllUrlFiltersRemoved, target: selectedProfileAllUrlFiltersRemovedFx });
+sample({ clock: selectedProfileAllUrlFiltersRemovedFx.doneData, target: profileUpdated });
