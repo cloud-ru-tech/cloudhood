8338ef9007d865734ed1bd23ccd0abd198a7a672
kirrrrusha <kirilllebedenko@gmail.com>
2025-09-14 13:40:24 +0300

    chore: update packages

--- src/widgets/request-headers/components/RequestHeaderRow/RequestHeaderRow.tsx
+++ src/widgets/request-headers/components/RequestHeaderRow/RequestHeaderRow.tsx
@@ -86,6 +86,7 @@ export function RequestHeaderRow(props: RequestHeader) {
             showClearButton={false}
             disabled={isPaused}
             validationState={name.length > 0 && !isNameFormatVerified ? 'error' : 'default'}
+            inputMode='text'
           />
         </Tooltip>
       </S.LeftHeaderActions>
@@ -105,6 +106,7 @@ export function RequestHeaderRow(props: RequestHeader) {
           showClearButton={false}
           disabled={isPaused}
           validationState={value.length > 0 && !isValueFormatVerified ? 'error' : 'default'}
+          inputMode='text'
         />
       </Tooltip>
 
