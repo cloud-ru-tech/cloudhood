307a5c26cabbfeb245cee54544a5dd9a57b744d3
Kirill Lebedenko <kialebedenko@cloud.ru>
2025-09-21 15:42:01 +0300

    feat: add tabs system for ProfileActions
    - Add profile-actions entity with tab management
    - Update ProfileActions component to use tabs for Headers/URL Filters
    - Update header hooks to support tab switching when adding URL filters

--- src/pages/main/components/ProfileActions/ProfileActions.tsx
+++ src/pages/main/components/ProfileActions/ProfileActions.tsx
@@ -1,21 +1,20 @@
 import { useUnit } from 'effector-react';
-import { useState } from 'react';
 
 import { Tabs } from '@snack-uikit/tabs';
 
 import { $isPaused } from '#entities/is-paused/model';
+import { $activeProfileActionsTab, profileActionsTabChanged } from '#entities/profile-actions';
 
 import { RequestHeadersActions } from './RequestHeadersActions';
 import * as S from './styled';
 import { UrlFiltersActions } from './UrlFiltersActions';
 
 export function ProfileActions() {
-  const [isPaused] = useUnit([$isPaused]);
-  const [activeTab, setActiveTab] = useState('headers');
+  const [isPaused, activeTab] = useUnit([$isPaused, $activeProfileActionsTab]);
 
   return (
     <S.Content>
-      <Tabs value={activeTab} onChange={setActiveTab}>
+      <Tabs value={activeTab} onChange={profileActionsTabChanged}>
         <Tabs.TabBar type='secondary'>
           <Tabs.Tab label='Headers' value='headers' />
           <Tabs.Tab label='URL Filters' value='url-filters' />
