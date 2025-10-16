7b363ead2c16ff0c550bf6590ea1107a0c37bf41
Aleksandr Kovtunov <aakovtunov@cloud.ru>
2025-10-16 18:52:18 +0300

    feat: add active filters counter

--- src/entities/request-profile/model/selected-profile-url-filters.ts
+++ src/entities/request-profile/model/selected-profile-url-filters.ts
@@ -1,5 +1,7 @@
 import { combine } from "effector";
 
+import { validateUrlFilter } from "#shared/utils/createUrlCondition";
+
 import { $requestProfiles } from "./request-profiles";
 import { $selectedRequestProfile } from "./selected-request-profile";
 
@@ -7,5 +9,11 @@ export const $selectedProfileUrlFilters = combine(
   $selectedRequestProfile,
   $requestProfiles,
   (selectedProfileId, profiles) => profiles.find(p => p.id === selectedProfileId)?.urlFilters ?? [],
+  { skipVoid: false },
+);
+
+export const $selectedProfileActiveUrlFiltersCount = combine(
+  $selectedProfileUrlFilters,
+  headers => headers.filter(item => !item.disabled && validateUrlFilter(item.value).isValid).length,
   { skipVoid: false },
 );
--- src/pages/main/components/ProfileActions/ProfileActions.tsx
+++ src/pages/main/components/ProfileActions/ProfileActions.tsx
@@ -4,20 +4,35 @@ import { Tabs } from '@snack-uikit/tabs';
 
 import { $isPaused } from '#entities/is-paused/model';
 import { $activeProfileActionsTab, profileActionsTabChanged } from '#entities/profile-actions';
+import { $selectedProfileActiveRequestHeadersCount, $selectedProfileActiveUrlFiltersCount } from '#entities/request-profile/model';
+import { getCounterProps } from '#shared/utils/getCounterProps';
 
 import { RequestHeadersActions } from './RequestHeadersActions';
 import * as S from './styled';
 import { UrlFiltersActions } from './UrlFiltersActions';
 
 export function ProfileActions() {
-  const [isPaused, activeTab] = useUnit([$isPaused, $activeProfileActionsTab]);
+  const [isPaused, activeTab, activeRequestHeadersCount, activeUrlFiltersCount] = useUnit([
+    $isPaused,
+    $activeProfileActionsTab,
+    $selectedProfileActiveRequestHeadersCount,
+    $selectedProfileActiveUrlFiltersCount
+  ]);
 
   return (
     <S.Content>
       <Tabs value={activeTab} onChange={profileActionsTabChanged}>
         <Tabs.TabBar type='secondary'>
-          <Tabs.Tab label='Headers' value='headers' />
-          <Tabs.Tab label='URL Filters' value='url-filters' />
+          <Tabs.Tab
+            counter={getCounterProps(activeRequestHeadersCount)}
+            label='Headers'
+            value='headers'
+          />
+          <Tabs.Tab
+            label='URL Filters'
+            counter={getCounterProps(activeUrlFiltersCount)}
+            value='url-filters'
+          />
         </Tabs.TabBar>
         <Tabs.TabContent value='headers'>
           <RequestHeadersActions />
